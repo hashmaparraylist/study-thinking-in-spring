71fb49c8e621a34a0bd16199d900d914d9f40ca9
Sebastian Qu <sebastianqu@gmail.com>
2020-08-03 14:45:34 +0800

    延迟初始化 Spring Bean

--- spring-bean/src/main/java/io/github/hashmaparraylist/bean/definition/BeanInitializationDemo.java
+++ spring-bean/src/main/java/io/github/hashmaparraylist/bean/definition/BeanInitializationDemo.java
@@ -2,10 +2,10 @@ package io.github.hashmaparraylist.bean.definition;
 
 import io.github.hashmaparraylist.bean.factory.DefaultUserFactory;
 import io.github.hashmaparraylist.bean.factory.UserFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 /**
  * Bean 初始化 示例
@@ -21,15 +21,17 @@ public class BeanInitializationDemo {
         applicationContext.register(BeanInitializationDemo.class);
         // 启动 Spring 应用上下文
         applicationContext.refresh();
-
+        // 非延迟初始化 在 Spring 应用上下文启动完成后被初始化
+        System.out.println("Spring 应用上下文已启动...");
         // 依赖查找
         UserFactory userFactory = applicationContext.getBean(UserFactory.class);
-
+        System.out.println(userFactory);
         // 关闭应用上下文
         applicationContext.close();
     }
 
     @Bean(initMethod = "initUserFactory")
+    @Lazy
     public UserFactory userFactory() {
         return new DefaultUserFactory();
     }
