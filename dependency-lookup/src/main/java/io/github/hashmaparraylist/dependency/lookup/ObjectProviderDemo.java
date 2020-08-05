1082a768dbce847f321dfbeb0e43b339c15995aa
Sebastian Qu <sebastianqu@gmail.com>
2020-08-05 16:02:13 +0800

    延迟依赖查找

--- dependency-lookup/src/main/java/io/github/hashmaparraylist/dependency/lookup/ObjectProviderDemo.java
+++ dependency-lookup/src/main/java/io/github/hashmaparraylist/dependency/lookup/ObjectProviderDemo.java
@@ -1,9 +1,11 @@
 package io.github.hashmaparraylist.dependency.lookup;
 
+import io.github.hashmaparraylist.ioc.overview.domain.User;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 /**
  * TODO
@@ -20,13 +22,33 @@ public class ObjectProviderDemo {
         applicationContext.refresh();
         // 依赖查找集合对象
         lookupByObjectProvider(applicationContext);
+
+        lookupIfAvailable(applicationContext);
+        lookupByStreamOps(applicationContext);
         // 关闭容器
         applicationContext.close();
     }
 
     @Bean
+    @Primary
     public String helloWorld() {    // 方法名称是 Bean 名称 = "helloWorld"
         return "Hello world!";
+    }
+
+    @Bean
+    public String messages() {
+        return "Message";
+    }
+
+    private static void lookupIfAvailable(ApplicationContext applicationContext) {
+        ObjectProvider<User> userObjectProvider = applicationContext.getBeanProvider(User.class);
+        User user = userObjectProvider.getIfAvailable(User::createUser);
+        System.out.println("当前 user 对象: " + user);
+    }
+
+    private static void lookupByStreamOps(ApplicationContext applicationContext) {
+        ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
+        objectProvider.stream().forEach(System.out::println);
     }
 
     private static void lookupByObjectProvider(ApplicationContext applicationContext) {
