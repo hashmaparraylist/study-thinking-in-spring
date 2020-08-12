be6629bc647b1b6d218c38e488f089bc394e0f67
Sebastian Qu <sebastianqu@gmail.com>
2020-08-12 14:33:46 +0800

    集合类型注入

--- ioc-container-overview/src/main/java/io/github/hashmaparraylist/ioc/overview/domain/User.java
+++ ioc-container-overview/src/main/java/io/github/hashmaparraylist/ioc/overview/domain/User.java
@@ -3,14 +3,40 @@ package io.github.hashmaparraylist.ioc.overview.domain;
 import io.github.hashmaparraylist.ioc.overview.enums.City;
 import org.springframework.core.io.Resource;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  */
 public class User {
     private String name;
+
     private Long id;
+
     private City city;
+
+    private City[] workCities;
+
+    private List<City> lifeCities;
+
     private Resource configFileLocation;
+
+    public List<City> getLifeCities() {
+        return lifeCities;
+    }
+
+    public void setLifeCities(List<City> lifeCities) {
+        this.lifeCities = lifeCities;
+    }
+
+    public City[] getWorkCities() {
+        return workCities;
+    }
+
+    public void setWorkCities(City[] workCities) {
+        this.workCities = workCities;
+    }
 
     public Resource getConfigFileLocation() {
         return configFileLocation;
@@ -50,6 +76,8 @@ public class User {
                 "name='" + name + '\'' +
                 ", id=" + id +
                 ", city=" + city +
+                ", workCities=" + Arrays.toString(workCities) +
+                ", lifeCites=" + lifeCities +
                 ", configFileLocation=" + configFileLocation +
                 '}';
     }
