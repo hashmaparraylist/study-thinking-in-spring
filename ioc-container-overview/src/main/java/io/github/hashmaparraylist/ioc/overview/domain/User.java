83103f586e399eb404ed20d2b094913321c7cd43
Sebastian Qu <sebastianqu@gmail.com>
2020-08-12 14:06:12 +0800

    基础类型的注入

--- ioc-container-overview/src/main/java/io/github/hashmaparraylist/ioc/overview/domain/User.java
+++ ioc-container-overview/src/main/java/io/github/hashmaparraylist/ioc/overview/domain/User.java
@@ -1,11 +1,32 @@
 package io.github.hashmaparraylist.ioc.overview.domain;
 
+import io.github.hashmaparraylist.ioc.overview.enums.City;
+import org.springframework.core.io.Resource;
+
 /**
  *
  */
 public class User {
     private String name;
     private Long id;
+    private City city;
+    private Resource configFileLocation;
+
+    public Resource getConfigFileLocation() {
+        return configFileLocation;
+    }
+
+    public void setConfigFileLocation(Resource configFileLocation) {
+        this.configFileLocation = configFileLocation;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
 
     public String getName() {
         return name;
@@ -28,6 +49,8 @@ public class User {
         return "User{" +
                 "name='" + name + '\'' +
                 ", id=" + id +
+                ", city=" + city +
+                ", configFileLocation=" + configFileLocation +
                 '}';
     }
 
@@ -35,6 +58,7 @@ public class User {
         User user = new User();
         user.setId(2L);
         user.setName("createUser");
+        user.setCity(City.BEIJING);
         return user;
     }
 }
---
+++ ioc-container-overview/src/main/java/io/github/hashmaparraylist/ioc/overview/enums/City.java
@@ -0,0 +1,13 @@
+package io.github.hashmaparraylist.ioc.overview.enums;
+
+/**
+ * 城市枚举
+ *
+ * @author
+ * @date
+ */
+public enum City {
+    BEIJING,
+    SHANGHAI,
+    HANGZHOU
+}
