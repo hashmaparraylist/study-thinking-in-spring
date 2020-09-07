1d1f8d74149d50a8e7f5fea7032a71ed68b64518
Sebastian Qu <sebastianqu@gmail.com>
2020-09-07 14:26:36 +0800

    Bean 初始化前阶段

---
+++ bean-lifecycle/src/main/java/io/github/hashmaparraylist/ioc/bean/lifecycle/BeanInitializationLifecycleDemo.java
@@ -0,0 +1,42 @@
+package io.github.hashmaparraylist.ioc.bean.lifecycle;
+
+import io.github.hashmaparraylist.ioc.overview.domain.User;
+import org.springframework.beans.factory.support.BeanDefinitionReader;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Bean 初始化化生命周期示例
+ *
+ * @author
+ * @date 2020/9/2
+ */
+public class BeanInitializationLifecycleDemo {
+    public static void main(String[] args) {
+        executeBeanFactory();
+    }
+
+    public static void executeBeanFactory() {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        // 方法一:添加 BeanPostProcessor 的实现 MyInstantiationAwareBeanPostProcessor
+        beanFactory.addBeanPostProcessor(new MyInstantiationAwareBeanPostProcessor());
+        // 方法二:MyInstantiationAwareBeanPostProcessor 作为 Bean 注册
+        // 基于 XML 资源的 BeanDefinitionReader 的实现
+        BeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+        String[] locations = {"classpath:META-INF/dependency-lookup-context.xml", "classpath:META-INF/bean-constructor-dependency-injection.xml"};
+        int beanDefinitionCount = beanDefinitionReader.loadBeanDefinitions(locations);
+        System.out.printf("已加载 BeanDefinition 数量: %d%n", beanDefinitionCount);
+
+        User user = beanFactory.getBean("user", User.class);
+        System.out.println(user);
+
+        User superUser = beanFactory.getBean("superUser", User.class);
+        System.out.println(superUser);
+
+        // 构造器注入是按照类型去选择参数， resolveDependency
+        UserHolder userHolder = beanFactory.getBean("userHolder", UserHolder.class);
+        System.out.println(userHolder);
+    }
+}
+
--- bean-lifecycle/src/main/java/io/github/hashmaparraylist/ioc/bean/lifecycle/MyInstantiationAwareBeanPostProcessor.java
+++ bean-lifecycle/src/main/java/io/github/hashmaparraylist/ioc/bean/lifecycle/MyInstantiationAwareBeanPostProcessor.java
@@ -58,4 +58,13 @@ class MyInstantiationAwareBeanPostProcessor implements InstantiationAwareBeanPos
 
         return null;
     }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (ObjectUtils.nullSafeEquals("userHolder", beanName) && UserHolder.class.equals(bean.getClass())) {
+            UserHolder userHolder = (UserHolder) bean;
+            userHolder.setDescription("The user holder V3.");
+        }
+        return bean;
+    }
 }
