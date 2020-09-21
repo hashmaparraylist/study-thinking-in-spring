b8600e2dc65d5928ced0bd5a8c17313c5609f8c7
Sebastian Qu <sebastianqu@gmail.com>
2020-09-21 15:00:30 +0800

    带有字符编码的FileSystemResourceLoader示例

--- resource/src/main/java/io/github/hashmaparraylist/resource/EncodedFileSystemResourceLoaderDemo.java
+++ resource/src/main/java/io/github/hashmaparraylist/resource/EncodedFileSystemResourceLoaderDemo.java
@@ -2,6 +2,7 @@ package io.github.hashmaparraylist.resource;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.FileSystemResourceLoader;
 import org.springframework.core.io.support.EncodedResource;
 
 import java.io.File;
@@ -9,20 +10,22 @@ import java.io.IOException;
 import java.io.Reader;
 
 /**
- * 带有字符编码的 {@link FileSystemResource} 示例
+ * 带有字符编码的 {@link FileSystemResourceLoader} 示例
  *
  *
  * @author
  * @date 2020/9/21
+ * @see FileSystemResourceLoader
  * @see FileSystemResource
  * @see EncodedResource
  */
-public class EncodedFileSystemResourceDemo {
+public class EncodedFileSystemResourceLoaderDemo {
     public static void main(String[] args) throws IOException {
-        String currentJavaFilePath = System.getProperty("user.dir") + "/resource/src/main/java/io/github/hashmaparraylist/resource/EncodedFileSystemResourceDemo.java";
-        File currentJavaFile = new File(currentJavaFilePath);
+        String currentJavaFilePath = "/" + System.getProperty("user.dir") + "/resource/src/main/java/io/github/hashmaparraylist/resource/EncodedFileSystemResourceLoaderDemo.java";
+        // 新建一个 FileSystemResourceLoader 的对象
+        FileSystemResourceLoader resourceLoader = new FileSystemResourceLoader();
         // FileSystemResource => WritableResource => Resource
-        FileSystemResource fsr = new FileSystemResource(currentJavaFile);
+        FileSystemResource fsr = (FileSystemResource) resourceLoader.getResource(currentJavaFilePath);
         EncodedResource er = new EncodedResource(fsr, "UTF-8");
         // 字符输入流
         try (Reader reader = er.getReader()) {
