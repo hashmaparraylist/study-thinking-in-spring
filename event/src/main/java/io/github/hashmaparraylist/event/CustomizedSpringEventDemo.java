f58eae3b46c0b5728469b2b386ef0abbd5135af1
Sebastian Qu <sebastianqu@gmail.com>
2020-11-06 16:44:25 +0800

    事件/监听器实现原理

--- event/src/main/java/io/github/hashmaparraylist/event/CustomizedSpringEventDemo.java
+++ event/src/main/java/io/github/hashmaparraylist/event/CustomizedSpringEventDemo.java
@@ -1,5 +1,7 @@
 package io.github.hashmaparraylist.event;
 
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.support.GenericApplicationContext;
 
 /**
@@ -15,12 +17,19 @@ public class CustomizedSpringEventDemo {
 
         // 1. 添加自定义ApplicationListener
         context.addApplicationListener(new MySpringEventListener());
+        context.addApplicationListener(new ApplicationListener<ApplicationEvent>() {
+            @Override
+            public void onApplicationEvent(ApplicationEvent event) {
+                System.out.println(event);
+            }
+        });
 
         // 2. 启动 Spring 应用上下文
         context.refresh();
 
         // 3. 发送自定义 Spring 事件
         context.publishEvent(new MySpringEvent("Hello, world!"));
+        context.publishEvent(new MySpringEventSub("2020"));
 
         // 4. 关闭 Spring 应用上下文
         context.close();
