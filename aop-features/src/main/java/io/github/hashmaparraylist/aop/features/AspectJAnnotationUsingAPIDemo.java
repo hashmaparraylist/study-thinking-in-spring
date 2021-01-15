12732da728b48d239d00e7c65dd4181395bd9394
Sebastian Qu <sebastianqu@gmail.com>
2021-01-15 11:26:04 +0800

    编程方式创建 AspectJ 代理示例

---
+++ aop-features/src/main/java/io/github/hashmaparraylist/aop/features/AspectJAnnotationUsingAPIDemo.java
@@ -0,0 +1,46 @@
+package io.github.hashmaparraylist.aop.features;
+
+import io.github.hashmaparraylist.aop.features.aspect.AspectConfiguration;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.aop.MethodBeforeAdvice;
+import org.springframework.aop.aspectj.annotation.AspectJProxyFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ *
+ * @author
+ * @date 2021/1/15
+ */
+public class AspectJAnnotationUsingAPIDemo {
+    public static void main(String[] args) {
+        // 创建一个 HashMap 缓存, 作为被代理对象
+        Map<String, Object> cache = new HashMap<>();
+        // 创建 ProxyFactory (AspectJ)
+        AspectJProxyFactory proxyFactory = new AspectJProxyFactory(cache);
+        // 增加 Aspect 配置类
+        proxyFactory.addAspect(AspectConfiguration.class);
+
+        proxyFactory.addAdvice(new MethodBeforeAdvice() {
+            @Override
+            public void before(Method method, Object[] args, Object target) throws Throwable {
+                if ("put".equals(method.getName()) && args.length == 2) {
+                    System.out.printf("当前存放是 Key : %s , Value : %s \n", args[0], args[1]);
+                }
+            }
+        });
+
+        // 存储数据
+        // cache.put("1", "A");
+        // 通过代理对象存储
+        Map<String, Object> proxy = proxyFactory.getProxy();
+        proxy.put("1", "A");
+        System.out.println(cache.get("1"));
+    }
+}
---
+++ aop-features/src/main/java/io/github/hashmaparraylist/aop/features/aspect/AspectConfiguration.java
@@ -0,0 +1,13 @@
+package io.github.hashmaparraylist.aop.features.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * TODO
+ *
+ * @author
+ * @date 2021/1/15
+ */
+@Aspect
+public class AspectConfiguration {
+}
