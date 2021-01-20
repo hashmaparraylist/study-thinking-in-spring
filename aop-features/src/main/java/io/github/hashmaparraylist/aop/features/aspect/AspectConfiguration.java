71932133da36dde275f5cbb0040bff5a4a844a31
Sebastian Qu <sebastianqu@gmail.com>
2021-01-20 15:40:30 +0800

    Around 注解示例

--- aop-features/src/main/java/io/github/hashmaparraylist/aop/features/aspect/AspectConfiguration.java
+++ aop-features/src/main/java/io/github/hashmaparraylist/aop/features/aspect/AspectConfiguration.java
@@ -1,5 +1,7 @@
 package io.github.hashmaparraylist.aop.features.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -16,6 +18,11 @@ public class AspectConfiguration {
     @Pointcut("execution(public * *(..))")
     private void anyPublicMethod() {
         System.out.println("@Pointcut any public method.");
+    }
+    @Around("anyPublicMethod")
+    public Object aroundAnyPublicMethod(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("@Around any public method");
+        return pjp.proceed();
     }
 
     @Before("anyPublicMethod()")
