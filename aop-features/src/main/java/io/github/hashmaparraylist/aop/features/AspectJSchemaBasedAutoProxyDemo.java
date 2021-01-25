a2628b5ee65dd6fff861a2601fda928b56dd3fcc
Sebastian Qu <sebastianqu@gmail.com>
2021-01-25 16:02:59 +0800

    自动动态代理示例

---
+++ aop-features/src/main/java/io/github/hashmaparraylist/aop/features/AspectJSchemaBasedAutoProxyDemo.java
@@ -0,0 +1,30 @@
+package io.github.hashmaparraylist.aop.features;
+
+import io.github.hashmaparraylist.aop.overview.EchoService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 基于 XML 配置Pointcut 示例
+ *
+ * @author
+ * @date 2021/1/15
+ */
+@Configuration          // Configuration class
+@EnableAspectJAutoProxy // 激活 AspectJ 注解自动代理
+public class AspectJSchemaBasedAutoProxyDemo {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("classpath:/META-INF/spring-aop-auto-proxy-context.xml");
+
+        context.refresh();
+
+        EchoService echoService = context.getBean("echoService", EchoService.class);
+
+        System.out.println(echoService.echo("Hello, world."));
+
+        context.close();
+    }
+
+}
