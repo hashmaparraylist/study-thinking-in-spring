f5b740cbeb8dcc47872f623918af00c2ef8bfc16
Sebastian Qu <sebastianqu@gmail.com>
2021-01-25 14:10:32 +0800

    基于 XML 配置的后置动作

--- aop-features/src/main/java/io/github/hashmaparraylist/aop/features/aspect/AspectXmlConfig.java
+++ aop-features/src/main/java/io/github/hashmaparraylist/aop/features/aspect/AspectXmlConfig.java
@@ -2,6 +2,8 @@ package io.github.hashmaparraylist.aop.features.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 
+import java.util.Random;
+
 /**
  * TODO
  *
@@ -14,7 +16,24 @@ public class AspectXmlConfig {
     }
 
     public Object aroundAnyPublicMethod(ProceedingJoinPoint pjp) throws Throwable {
+        Random random = new Random();
+        if (random.nextBoolean()) {
+            throw new RuntimeException("For Purpose from XML configuration.");
+        }
         System.out.println("Around any public method : " + pjp.getSignature());
         return pjp.proceed();
     }
+
+    public void finalizeAnyPublicMethod() {
+        System.out.println("@After any public method");
+    }
+
+    public void afterAnyPublicMethod() {
+        System.out.println("@AfterReturning any public method");
+    }
+
+    public void afterThrowingAnyPublicMethod() {
+        System.out.println("@AfterThrowing any public method");
+    }
+
 }
